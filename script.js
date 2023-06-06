dd4de8fe8e12d5d14a1035e6101565012eb53e87
Shailee-11 <128290114+Shailee-11@users.noreply.github.com>
2023-06-06 14:36:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
     //Write your code here
-
-
+	var bold=document.getElementsByTagName("strong");
+	for(let i=0;i<bold.length;i++){
+		bold[i].classList.add("highlight");
+	}
 }
 
 
 function return_normal() {
     //Write your code here
-
+	var bold=document.getElementsByTagName("strong");
+     for(let i=0;i<bold.length;i++){
+		 bold[i].classList.remove("highlist");
+	 }
     
 }
